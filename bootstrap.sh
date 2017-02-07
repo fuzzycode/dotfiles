019158efd8182afb8dd4524a49822956e6f85329
Björn Larsson <b.e.larsson@gmail.com>
2017-02-07 17:16:24 +0100

    Sort package installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,13 +21,8 @@ brew install ack
 brew install aspell
 brew install bash-completion
 brew install cask
-brew install sphinx-doc
-brew install cmake
-brew install ninja
-brew install openssl
-brew install sqlite
-brew install python
 brew install clang-format
+brew install cmake
 brew install editorconfig
 brew install fasd
 brew install flake8
@@ -36,19 +31,24 @@ brew install git
 brew install git-extras
 brew install git-flow
 brew install global
+brew install homebrew/emacs/tern -with-auto-complete
+brew install homebrew/fuse/sshfs
+brew install hub
 brew install hunspell
 brew install markdown
+brew install ninja
 brew install node
+brew install openssl
+brew install python
 brew install python3
-brew install rtags
-brew install the_silver_searcher
+brew install railwaycat/emacsmacport/emacs-mac -with-spacemacs-icon
 brew install ripgrep
+brew install rtags
+brew install sphinx-doc
+brew install sqlite
+brew install the_silver_searcher
 brew install wget
 brew install zsh
-brew install homebrew/fuse/sshfs
-brew install homebrew/emacs/tern -with-auto-complete
-brew install railwaycat/emacsmacport/emacs-mac -with-spacemacs-icon
-brew install hub
 
 #Install all casks
 brew cask install font-inconsolata
