2d65f7413fbcbd30756d77f68e92e6ae9be48385
Björn Larsson <b.e.larsson@gmail.com>
2018-11-03 21:51:37 +0100

    Add interactive process kill command

--- zsh/funcs.zsh
+++ zsh/funcs.zsh
@@ -32,7 +32,23 @@ bcp() {
     fi
 }
 
+# cc - cd into child directory
 cc() {
     local dir=$(fd --full-path ${1:-.} -t d --color never | fzf +m --header='[change directory]')
     cd "$dir"
+}
+
+# fkill - kill processes
+fkill() {
+    local pid
+    if [ "$UID" != "0" ]; then
+        pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
+    else
+        pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+    fi
+
+    if [ "x$pid" != "x" ]
+    then
+        echo $pid | xargs kill -${1:-9}
+    fi
 }
