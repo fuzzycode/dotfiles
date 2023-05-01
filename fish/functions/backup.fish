df876911785d0e92e036a4ee3be4c6d354cfbee9
Björn Larsson <b.e.larsson@gmail.com>
2023-05-01 21:57:30 +0200

    Add description

--- fish/functions/backup.fish
+++ fish/functions/backup.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
 # Copies the given file but give it a .bak extension
-function backup --argument filename
+function backup --argument filename --description "Copy given file and append .bak to the name"
     cp $filename $filename.bak
 end
