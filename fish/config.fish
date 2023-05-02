54bee98cc185d6edb4d59e418c8ff6eca448d26e
Björn Larsson <b.e.larsson@gmail.com>
2023-05-02 20:18:44 +0200

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -5,14 +5,18 @@ set -Ux fisher_path ~/.local/share/fish
 # Add homebrew directories to path
 fish_add_path /opt/homebrew/bin/
 
-# Set locale
-set -x LANG en_US.UTF-8
+# Add my custom git scripts to path
+fish_add_path ~/.dotfiles/git/scripts
+
 #clear greeting message
 set -U fish_greeting
 
+set -x LANG en_US.UTF-8
 set -x RIPGREP_CONFIG_PATH ~/.ripgreprc
 set -x HOMEBREW_NO_ENV_HINTS 1
 set -x XDG_DATA_HOME ~/.local/share
+set -x XDG_CONFIG_HOME ~/.config
+set -x EDITOR emacsclient
 
 # Setup the interactive shell
 if status is-interactive
@@ -23,6 +27,16 @@ if status is-interactive
     abbr --add ls exa
     abbr --add tree exa --tree
     abbr --add cdr cd-gitroot
+
+
+    # Aliases
+    alias c clear
+    alias gs 'git status'
+    alias gd 'git diff'
+
+    # FZF config
+    set fzf_history_opts --with-nth=4.. # Remove date/time display
+
 end
 
 # Load local config if it exists
